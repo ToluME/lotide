0e195ef24495ec8f8e74289d9613f42570fe7204
Tolu Eshiet <tmeshiet@gmail.com>
2023-05-28 00:20:13 -0600

    Callback returns truthy value

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+      
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+      
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log("✅✅✅ Arrays are equal:", array1, "==", array2);
+  } else {
+    console.log("🛑🛑🛑 Arrays are not equal:", array1, "!=", array2);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return result;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
