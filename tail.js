464165e23af1d7be73426edc3a1beb19a0694db6
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 15:29:45 -0600

    code refractor to dry

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(a) {
   if (a.length <= 0) {
@@ -13,16 +7,4 @@ const tail = function(a) {
   return a.slice(1);
 };
 
-assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
-assertEqual(tail(['a', 'b', 'c']).toString(), ['b', 'c'].toString());
-assertEqual(tail([]).toString(), [].toString());
-assertEqual(tail([1]).toString(), [].toString());
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
