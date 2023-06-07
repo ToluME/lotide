aadaa0cb284acd2ee0cc8f1c42639ab2b386c1c5
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 15:21:27 -0600

    DRY-ing up code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 2);
-assertEqual(10, 7);
-assertEqual("Talia", "Talia");
+
+
+module.exports = assertEqual;
 
