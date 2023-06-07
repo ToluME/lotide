8adcdf927228a4d586f3cb63b6cada2f95f70a77
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 15:51:50 -0600

    Code refractor export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log("✅✅✅ Arrays are equal:", array1, "==", array2);
@@ -7,21 +7,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
   
-// Copied eqArray function
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-    
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-    
-  return true;
-};
+module.exports = assertArraysEqual;
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
