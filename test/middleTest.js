72158480e26d53ffb189f5fecb62464f79e51c16
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 16:33:32 -0600

    Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert;
+const middle  = require('../middle');
 
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1]));
-
-
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), [1]);
+describe("#middle", () => {
+  it("returns  [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
+  
+it("returns [5] for [5]", () => {
+  assert.deepEqual(middle([5]), [5]);
+});
