9d3837aa721c98eef67c8ab67c123de88eb6f8b1
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 16:20:30 -0600

    Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -10,10 +10,3 @@ describe("#head", () => {
 it("returns '5' for ['5']", () => {
   assert.strictEqual(head(['5']), '5'); 
 });
-
-// const assertEqual = require('../assertEqual');
-// const head = require('../head');
-//Test without mocha
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([1]),1);
