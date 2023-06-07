ca4ad9c62b6548845e0da3c99b855db519dea5b7
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 16:06:20 -0600

    Refractor

--- middle.js
+++ middle.js
@@ -1,27 +1,6 @@
-/* eslint-disable func-style */
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-        
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-        
-  return true;
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
-function assertArraysEqual(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log("✅✅✅ Arrays are equal:", array1, "==", array2);
-  } else {
-    console.log("🛑🛑🛑 Arrays are not equal:", array1, "!=", array2);
-  }
-}
-
-function middle(array) {
+const middle = function(array) {
   const length = array.length;
   const middleIndex = Math.floor(length / 2);
   
@@ -30,11 +9,7 @@ function middle(array) {
   } else {
     return array.slice(middleIndex, middleIndex + 1);
   }
-}
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1]));
+};
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), [1]);
+
+module.exports = middle;
