b80853defc92dec7ec846e2d4f90585ab4b4542f
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-07 16:49:01 -0600

    Refractor with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,34 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
-assertEqual(tail(['a', 'b', 'c']).toString(), ['b', 'c'].toString());
-assertEqual(tail([]).toString(), [].toString());
-assertEqual(tail([1]).toString(), [].toString());
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+it("returns [] for [1]", () => {
+  assert.deepEqual(tail([1]), []);
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+it("returns ['b', 'c'] for ['a', 'b', 'c']", () => {
+  assert.deepEqual(tail(['a', 'b', 'c']), ['b', 'c']);
+});
+
+
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
+// assertEqual(tail(['a', 'b', 'c']).toString(), ['b', 'c'].toString());
+// assertEqual(tail([]).toString(), [].toString());
+// assertEqual(tail([1]).toString(), [].toString());
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
