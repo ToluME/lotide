d643e319f6f9d100fddc763f37b779c30d68efee
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-08 07:47:37 -0600

    Dry

--- without.js
+++ without.js
@@ -1,5 +1,7 @@
-/* eslint-disable func-style */
-function without(source, itemsToRemove) {
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+const without = function(source, itemsToRemove) {
   const result = [];
   
   for (let item of source) {
@@ -9,33 +11,9 @@ function without(source, itemsToRemove) {
   }
   
   return result;
-}
+};
+
+module.exports = without;
+
 console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
-
-
-function assertArraysEqual(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log("✅✅✅ Arrays are equal:", array1, "==", array2);
-  } else {
-    console.log("🛑🛑🛑 Arrays are not equal:", array1, "!=", array2);
-  }
-}
-const words = ["hello", "world", "lighthouse"];
-console.log(without(words, ["lighthouse"]));
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-    
-// Copied eqArray function
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-      
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-      
-  return true;
-}
