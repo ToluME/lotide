2a1f95e028d1a8048b5fe2af00a0d7db8d52f8c7
Tolu Eshiet <tmeshiet@gmail.com>
2023-06-10 10:46:54 -0600

    Refractored for lengthy unused comments and required functions

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,10 +1,5 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const letterPositions = require('../letterPositions');
 
-// const asking = "hello";
-// const result1 = letterPositions(asking);
-// //console.log(result1);
-
-
 const result1 = letterPositions("hello").e;
 assertArraysEqual(result1, [1]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -14,21 +14,3 @@ it("returns [] for [1]", () => {
 it("returns ['b', 'c'] for ['a', 'b', 'c']", () => {
   assert.deepEqual(tail(['a', 'b', 'c']), ['b', 'c']);
 });
-
-
-// const assertEqual = require('../assertEqual');
-// const tail = require('../tail');
-
-// assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString());
-// assertEqual(tail(['a', 'b', 'c']).toString(), ['b', 'c'].toString());
-// assertEqual(tail([]).toString(), [].toString());
-// assertEqual(tail([1]).toString(), [].toString());
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
